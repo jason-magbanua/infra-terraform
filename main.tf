19a86850c119651295e897104cdd1f595e9ff2c5
19658514+jason-magbanua@users.noreply.github.com <jason-magbanua>
2026-03-31 05:55:33 +0000

    Removed hashicorp vault

--- main.tf
+++ main.tf
@@ -74,9 +74,6 @@ locals {
     postgresql    = { cores = 2, memory = 2048, disk = 20, network = { address = "10.10.200.30/24", gateway = local.gw } }
     redis         = { cores = 1, memory = 512,  disk = 4,  network = { address = "10.10.200.31/24", gateway = local.gw } }
 
-    # --- Security & Identity -------------------------------------------
-    hashicorp-vault = { cores = 2, memory = 1024, disk = 8, network = { address = "10.10.200.40/24", gateway = local.gw } }
-
   }
 
   containers = {
